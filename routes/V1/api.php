514d8e9a250827df7eb1efe090d7e25826437a8b
paulo93f <paulo93f@gmail.com>
2024-05-19 20:00:17 +0200

    Added endpoint to get tickets from a current user id.

--- routes/V1/api.php
+++ routes/V1/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthorController;
 
 
-Route::middleware('auth:sanctum')->apiResource('/tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 
-Route::middleware('auth:sanctum')->apiResource('/authors', AuthorController::class);
-
+Route::middleware('auth:sanctum')->apiResource('authors', AuthorController::class);
+Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
