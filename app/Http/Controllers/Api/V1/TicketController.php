8752134a59ff9860bb3cee03ae20c5a744cc3c89
paulo93f <paulo93f@gmail.com>
2024-06-01 13:32:52 +0200

    Implemented store ticekts for tickets and authors request. Added validations at this requests.

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
@@ -15,7 +17,6 @@ class TicketController extends ApiController
      */
     public function index(TicketFilter $filter)
     {
-
         return TicketResource::collection(Ticket::filter($filter)->paginate());
     }
 
@@ -24,7 +25,22 @@ class TicketController extends ApiController
      */
     public function store(StoreTicketRequest $request)
     {
-        //
+        try {
+            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+        }catch (ModelNotFoundException $exception){
+            return $this->ok('User not found', [
+                'error' => 'The provided user id does not exist.'
+            ]);
+        }
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $user->id,
+        ];
+
+       return new TicketResource(Ticket::create($model));
     }
 
     /**
